887b10202204cd6863ee522de593d1dc42b59950
AlexKiprono <lexiekiprono@gmail.com>
2024-04-11 21:38:57 +0300

    deploy

--- index.js
+++ index.js
@@ -25,18 +25,9 @@ document.addEventListener('DOMContentLoaded', function(){
               <p class="listTimeLine">${post.listTimeLine}</p>
               <div class="last-section d-flex justify-content-between m-0">
                   <h6 class="listPrice" style="color:green;" >$${post.price}</h6>
-
-                  <div class="likes-section" data-image-id="${post.id}">
-                  <span class="likes">${post.likesCount} likes</span>
-                  <i class="fa fa-heart like-button" aria-hidden="true"></i>
-                  </div>
-
+                  <p class="ratings">${post.ratings}</p>
               </div>
           </div>
-
-
-          <a href="#" class="btn text-decoration-none btn-primary mb-3 viewAll" id="viewAll" data-bs-toggle="modal" data-bs-target="#viewAllDetails" data-postId="${post.id}">View more...</a>
-        
         
         </div>
       `;
@@ -46,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function(){
   }
 
   // Fetch properties and render them
-  fetch("http://localhost:3000/properties")
+  fetch("http://localhost:5000/properties")
     .then((response) => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
@@ -57,104 +48,7 @@ document.addEventListener('DOMContentLoaded', function(){
     .then((posts) => renderProperty(posts))
 
     .catch((error) => console.error('Error fetching data:', error));
-
-
-
-        // update likes
-    document.addEventListener('click',(e) => {
-        if (e.target.classList.contains('like-button')) {
-            const likeButton = e.target;
-            const likesSection = likeButton.closest('.likes-section');
-            const postId = likesSection.dataset.postId;
-            const likesSpan = likesSection.querySelector('.likes');
-            let likes = parseInt(likesSpan.textContent);
-            if (!isNaN(likes)) {
-                likes++;
-                updateLikes(postId, likes);
-                likesSpan.textContent = `${likes} likes`;
-            }
-        }
-    });
-
-
-    // Function to update likes on the server and in the page
-    function updateLikes(postId, likes) {
-      fetch(`http://localhost:3000/properties/${postId}`, {
-          method: 'PATCH',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ likes: likes }),
-      })
-      .then(response => response.json())
-      .then(updatedPost => {
-          const likesCount = document.querySelector(`#like-count[${postId}"] .likes`);
-          likesCount.textContent = `${updatedPost.likes} likes`;
-      })
-      .catch(error => console.error('Error updating likes:', error));
-    }
-
-
-
-    //view all data on a modal
-
-// Event listener for "viewAll" links
-document.addEventListener('DOMContentLoaded', function() {
-  // Event listener for "viewAll" links
-  document.querySelectorAll(".viewAll").forEach(link => {
-      link.addEventListener("click", (event) => {
-          event.preventDefault();
-          const postId = event.target.dataset.postid; // Corrected to dataset.postid
-          // Fetch the post data from your API using postId
-          fetch(`http://localhost:3000/properties/${postId}`)
-              .then(response => {
-                  if (!response.ok) {
-                      throw new Error('Failed to fetch post data');
-                  }
-                  return response.json();
-              })
-              .then(post => {
-                  // Populate modal with post details
-                  const modalBody = document.getElementById('viewAllDetails');
-                  modalBody.innerHTML = `
-                      <div class="card lh-1">
-                          <img src="${post.image}" class="card-img-top m-o" alt="${post.listName}">
-                          <div class="card-body">
-                              <h5 class="card-title">${post.listName}</h5>
-                              <p class="card-text">${post.listDescription}</p>
-                              <p class="card-text"><small class="text-muted">${post.listTimeLine}</small></p>
-                              <p class="card-text" style="color:green;">Price: $${post.price}</p>
-                          </div>
-                          <div class="modal-footer">
-                              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                          </div>
-
-                          <div class="mb-3">
-                          <label for="comments" class="form-label">Leave your Comments</label>
-                          <textarea class="form-control" id="comments" rows="3" placeholder=""></textarea>
-                        </div>
-                      </div>
-                  `;
-
-                  const viewAllModal = new bootstrap.Modal(document.getElementById('viewAllDetails'));
-                  viewAllModal.show();
-              })
-              .catch(error => console.error('Error fetching post data:', error));
-      });
-  });
 });
-
-
-
-
-
-
-
-});
-
-
-
-
 
 
 // Create Property list
@@ -167,9 +61,10 @@ document.addEventListener('DOMContentLoaded', function(){
     const listDescriptionValue = document.querySelector('#listDescription').value;
     const listTimeLineValue = document.querySelector('#listTimeLine').value;
     const priceValue = document.querySelector('#listPrice').value;
+    const ratings = document.querySelector('input[name="rating"]:checked').value; // Get the value of the selected radio button
 
     // Fetch existing properties to determine the maximum ID
-    fetch('http://localhost:3000/properties')
+    fetch('http://localhost:5000/properties')
       .then(response => {
         if (!response.ok) {
           throw new Error('Failed to fetch properties');
@@ -185,7 +80,7 @@ document.addEventListener('DOMContentLoaded', function(){
               
 
         // POST the new property with the incremented ID
-        return fetch('http://localhost:3000/properties', {
+        return fetch('http://localhost:5000/properties', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
@@ -196,7 +91,8 @@ document.addEventListener('DOMContentLoaded', function(){
             listName: listNameValue,
             listDescription: listDescriptionValue,
             listTimeLine: listTimeLineValue,
-            price: priceValue
+            price: priceValue,
+            ratings: ratings
           })
         });
       })
@@ -215,15 +111,15 @@ document.addEventListener('DOMContentLoaded', function(){
 
 //EDIT data in the json-server
 function editProperty(id) {
-  fetch(`http://localhost:3000/properties/${id}`)
+  fetch(`http://localhost:5000/properties/${id}`)
   .then((response) => response.json())
   .then((post) => {
     const editProperty = document.querySelector("#editPropertyForm");
 
     editProperty.innerHTML = `
-      <div class="input-group mb-3">
-        <label class="input-group-text" for="inputGroupFile01">Edit Image</label>
-        <input type="file" class="form-control" id="image_update" value="${post.image}" accept="images/*">
+      <div class="mb-3">
+      <label for="listName" class="form-label">Image</label>
+      <input type="url" class="form-control" id="image_update" value="${post.image}" placeholder="image">
       </div>
           
       <div class="mb-3">
@@ -246,6 +142,18 @@ function editProperty(id) {
         <input type="number" class="form-control" id="update_listPrice" value="${post.price}" placeholder="Price">
         <!-- Placeholder text updated for clarity -->
       </div>
+
+      <div class="mb-3">
+      <label for="ratings" class="form-label">Ratings</label>
+      <div class="edit-rating">
+          <input type="radio" id="star5" name="edit-rating" value="⭐⭐⭐⭐⭐"><label for="star5"></label>
+          <input type="radio" id="star4" name="edit-rating" value="⭐⭐⭐⭐✩"><label for="star4"></label>
+          <input type="radio" id="star3" name="edit-rating" value="⭐⭐⭐✩✩"><label for="star3"></label>
+          <input type="radio" id="star2" name="edit-rating" value="⭐⭐✩✩✩"><label for="star2"></label>
+          <input type="radio" id="star1" name="edit-rating" value="⭐✩✩✩✩"><label for="star1"></label>
+      </div>
+    </div>
+  
           
       <button onClick="update_post(${id})" type="submit" class="btn btn-primary" id="submit">Submit</button>
     `;
@@ -260,8 +168,9 @@ function update_post(id) {
   const listDescriptionValue = document.querySelector('#update_listDescription').value;
   const listTimeLineValue = document.querySelector('#update_listTimeLine').value;
   const priceValue = document.querySelector('#update_listPrice').value;
+  const ratings = document.querySelector('input[name="edit-rating"]:checked').value;
 
-  fetch(`http://localhost:3000/properties/${id}`, {
+  fetch(`http://localhost:5000/properties/${id}`, {
     method: "PATCH",
     headers: {
       "Content-Type": "application/json"
@@ -271,7 +180,8 @@ function update_post(id) {
       listName: listNameValue,
       listDescription: listDescriptionValue,
       listTimeLine: listTimeLineValue,
-      price: priceValue
+      price: priceValue,
+      ratings: ratings // Include the selected rating value in the request payload
     })
   })
   .then(() => {
@@ -284,7 +194,7 @@ function update_post(id) {
 function deleteProperty(id) {
   // Confirm with the user before deletion
   if (confirm("Are you sure you want to delete this property?")) {
-    fetch(`http://localhost:3000/properties/${id}`, {
+    fetch(`http://localhost:5000/properties/${id}`, {
       method: "DELETE"
     })
     .then(response => {
